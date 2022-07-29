7a992a7163685b2d1d02bd9c4b84bdd7cdb3c8cf
denissfjodorovs <ktelfon@gmail.com>
2022-07-29 20:35:13 +0300

    first commit

---
+++ src/main/java/lv/home/smallurl/SmallurlApplication.java
@@ -0,0 +1,13 @@
+package lv.home.smallurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/home/smallurl/SmallurlApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.home.smallurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
